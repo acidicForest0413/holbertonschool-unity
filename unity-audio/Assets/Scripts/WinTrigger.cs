812bbe711a337e5c413493597ce1134ea2e8754b
acidicForest0413 <5540@holbertonstudents.com>
2023-09-07 21:16:00 -0500

    update

--- unity-audio/Assets/Scripts/WinTrigger.cs
+++ unity-audio/Assets/Scripts/WinTrigger.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class WinTrigger : MonoBehaviour
 {
+    private AudioSource WinMusic;
     public Timer timer;
     public BackgroundMusicController bgmController;  // Reference to the music controller
 
+    void Start()
+    {
+        WinMusic = gameObject.GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -16,6 +22,7 @@ public class WinTrigger : MonoBehaviour
             timer.timerText.color = Color.green; // Change the color to green
 
             bgmController.StopMusic();  // Stop the background music
+            WinMusic.Play();
         }
     }
 }
